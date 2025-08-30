e82f00f651c65dadf6289ea0f3f44a69a0040c01
Thomas Korsgaard <tkorsgaard@gmail.com>
2025-08-30 19:11:23 +0200

    chapter 2 done

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from flask import render_template
 def index():
     user = {'username': 'Thomas'}
     title2 = {'title' : 'my blog'}
-    return render_template('index.html', user=user, title=title2)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, title=title2, posts=posts)
